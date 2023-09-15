72fe69bd619124c23790303c0010df442fd14eca
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:36:53 -0300

    olha o JS como vai

---
+++ aoba.js
@@ -0,0 +1,14 @@
+//comentário
+/*
+comentário
+*/
+alert("erro fatal");
+document.write("meu nome é Gustavo");
+document.write("meu nome é Gustavo");
+
+console.log("sabo mt, slk");
+console.log("sabo mt, slk");
+
+let tico = "foi msm, uau!!!!!!!!!!!!!!!!alegria"
+document.write(tico);
+console.log("aaa", tico, "aaaa", tico);
