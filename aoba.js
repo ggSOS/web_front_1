2867ca4a4728b09fd9d6613c37c8bd62e8215263
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:42:29 -0300

    exercício

--- aoba.js
+++ aoba.js
@@ -1,14 +1,4 @@
-//comentário
-/*
-comentário
-*/
-alert("erro fatal");
-document.write("meu nome é Gustavo");
-document.write("meu nome é Gustavo");
-
-console.log("sabo mt, slk");
-console.log("sabo mt, slk");
-
-let tico = "foi msm, uau!!!!!!!!!!!!!!!!alegria"
-document.write(tico);
-console.log("aaa", tico, "aaaa", tico);
+let tico = "Gustavo"
+let liru = "Gouvêa"
+console.log(tico);
+console.log(liru);
