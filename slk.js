db539865252524a005dc7c6e2142261b6e096141
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:00:46 -0300

    noitao dos brinquedos

--- slk.js
+++ slk.js
@@ -1,5 +1,11 @@
-function estaDoSeuAgrado() {
-    alert("olaaa");
-}
+const bot1 = document.querySelector(".bot1");
+const bot2 = document.querySelector(".bot2");
 
-estaDoSeuAgrado();
+const card = document.querySelector(".card");
+
+bot1.addEventListener("click", function(){
+    card.style.display = "none";
+})
+bot2.addEventListener("click", function(){
+    card.style.display = "block";
+})
